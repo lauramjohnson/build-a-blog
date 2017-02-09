cdbe067d0b5547e851a6f405396835ebe7e1fb52
Laura Johnson <lauramjohnson@me.com>
2017-02-08 22:58:41 -0600

    working newpost, started shell of blog post

--- main.py
+++ main.py
@@ -13,19 +13,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+
 import webapp2
-import os
+import cgi
 import jinja2
+import os
 
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
+class BlogPost(db.Model):
+    subject = db.StringProperty(required = True)
+    blog = db.TextProperty(required = True)
+    create = db.DateTimeProperty(auto_now_add = True)
+
 class MainHandler(webapp2.RequestHandler):
+    def render_front(self, subject="", blog="", error=""):
+        posts = db.GqlQuery("SELECT * FROM BlogPost")
+
+        t = jinja_env.get_template("newpost.html")
+        content = t.render(subject = subject, blog=blog, error=error, posts=posts)
+        self.response.write(content)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        subject = self.request.get("subject")
+        blog = self.request.get("blog")
+
+        if subject and blog:
+            a = BlogPost(subject=subject, blog=blog)
+            a.put()
+
+            self.redirect('/')
+        else:
+            error = "you must have a subject and a blog post"
+            self.render_front(subject, blog, error)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
